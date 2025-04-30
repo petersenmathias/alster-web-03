0038bee97edbf36aed6063b2f49443edde67e33f
Mathias Petersen <mathias.petersen@alster.se>
2025-04-30 15:38:24 +0200

    noindex nofollow

--- src/lib/components/Meta.svelte
+++ src/lib/components/Meta.svelte
@@ -19,6 +19,8 @@
 <svelte:head>
 	<meta charSet="utf-8" />
 
+	<meta name="robots" content="noindex" />
+
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 	<link rel="stylesheet" href="https://use.typekit.net/zjl6sdq.css" />
