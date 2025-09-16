d67b7ffa4d08f6f9721772b872119eef28cc55e3
Henrik Gånheim <henrik.ganheim@alster.se>
2025-09-16 09:33:48 +0200

    made the nav visible (wrong colors)

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -12,13 +12,13 @@
 </script>
 
 <div class="fixed top-0 z-20 p-6 mix-blend-difference">
-	<Logo wordMark={false} color="var(--color-background)" />
+	<Logo wordMark={false} color="var(--color-foreground)" />
 </div>
 <nav
 	class="absolute top-0 z-10 flex w-full flex-nowrap items-center justify-end p-6 mix-blend-difference"
 >
 	<ul
-		class="text-background flex w-full max-w-md flex-row justify-between gap-4 text-sm font-medium"
+		class="text-foreground flex w-full max-w-md flex-row justify-between gap-4 text-sm font-medium"
 	>
 		{#each links as link}
 			<li>
