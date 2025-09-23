68b978669ce9e915ad66b192e2fdf710b3fce13d
Henrik Gånheim <henrik.ganheim@alster.se>
2025-09-23 11:03:28 +0200

    Remove Logo component from Footer
    The Logo component and its associated markup have been removed from the Footer.svelte file. Layout classes were updated to reflect the absence of the logo, simplifying the footer's structure.

--- src/lib/blocks/Footer.svelte
+++ src/lib/blocks/Footer.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Grid from '$lib/components/Grid.svelte';
-	import Logo from '$lib/components/ui/Logo.svelte';
 	import { StoryblokComponent } from '@storyblok/svelte';
 	import type { ColumnStoryblok } from '../../types/storyblok';
 
@@ -18,18 +17,15 @@
 		{/each}
 
 		<div
-			class="col-span-3 row-span-1 row-start-auto mt-[15px] flex w-full flex-row items-center justify-between gap-4 self-end justify-self-end md:w-auto md:flex-col md:items-end md:justify-end"
+			class="absolute col-span-3 row-span-1 row-start-auto mt-[15px] flex w-full flex-col items-end justify-end gap-4 self-end justify-self-end"
 		>
 			<div
-				class="body text-md mr-auto flex max-w-lg flex-col gap-1 md:items-end md:justify-end md:self-end md:text-lg"
+				class="body text-md flex max-w-lg flex-col items-end justify-end gap-1 self-end md:text-lg"
 			>
 				<p>
 					&copy; {new Date().getFullYear()} Alster
 					<span class="relative -top-[10px] text-[6px]">TM</span>
 				</p>
-			</div>
-			<div class="relative -top-[5px] block flex-shrink-0 md:hidden">
-				<Logo color="var(--color-foreground)" />
 			</div>
 		</div>
 	</Grid>
