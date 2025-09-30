308087f838f57ff2509ea3879ebbf2948847fac7
Henrik Gånheim <henrik.ganheim@alster.se>
2025-09-30 09:20:47 +0200

    Update grid column spans and spacing in ThreeColumnItem
    Adjusted column spans for responsive layout and added margin and padding to improve spacing. Also set max-widths for Body components to enhance readability on larger screens.

--- src/lib/blocks/ThreeColumnItem.svelte
+++ src/lib/blocks/ThreeColumnItem.svelte
@@ -10,22 +10,22 @@
 </script>
 
 <div use:storyblokEditable={blok} class="flex w-full flex-col">
-	<Grid class="border-foreground-light flex-col border-t md:flex-row">
-		<div class="col-span-2">
-			<h2 class="text-foreground text-md md:text-lg">{blok.title}</h2>
+	<Grid class="border-foreground-light mb-12 flex-col border-t pt-5 md:flex-row">
+		<div class="col-span-6 md:col-span-2">
+			<h2 class="text-foreground text-md max-w-full md:text-lg">{blok.title}</h2>
 		</div>
-		<div class="col-span-3">
+		<div class="col-span-6 md:col-span-3">
 			<Body
 				markdown={blok.column1}
 				markdownRenderers={{ image: SbMarkdownImage }}
-				class="max-w-full"
+				class="max-w-full md:max-w-100"
 			/>
 		</div>
-		<div class="col-span-7">
+		<div class="col-span-6 md:col-span-7">
 			<Body
 				markdown={blok.column2}
 				markdownRenderers={{ image: SbMarkdownImage }}
-				class="max-w-full"
+				class="max-w-full md:max-w-170"
 			/>
 		</div>
 	</Grid>
