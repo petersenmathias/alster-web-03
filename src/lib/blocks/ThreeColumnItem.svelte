b3f70d87e55ed2d403fbee37a45c7bc705ec9839
Henrik Gånheim <henrik.ganheim@alster.se>
2025-10-03 11:41:18 +0200

    Update grid column spans and gaps in ThreeColumnItem
    Adjusted the column span classes for better layout responsiveness across breakpoints. Also updated Body component classes to use consistent gap utilities instead of max-width.

--- src/lib/blocks/ThreeColumnItem.svelte
+++ src/lib/blocks/ThreeColumnItem.svelte
@@ -11,21 +11,21 @@
 
 <div use:storyblokEditable={blok} class="flex w-full flex-col">
 	<Grid class="border-foreground-light mb-12 flex-col gap-y-10 border-t pt-5 md:flex-row">
-		<div class="col-span-6 md:col-span-2">
+		<div class="col-span-6 md:col-span-6 lg:col-span-1">
 			<h2 class="text-foreground text-md max-w-full md:text-lg">{blok.title}</h2>
 		</div>
-		<div class="col-span-6 md:col-span-3">
+		<div class="col-span-6 md:col-span-6 lg:col-span-2 lg:col-start-3">
 			<Body
 				markdown={blok.column1}
 				markdownRenderers={{ image: SbMarkdownImage }}
-				class="max-w-full md:max-w-100"
+				class="max-w-full gap-4 md:gap-6"
 			/>
 		</div>
-		<div class="col-span-6 md:col-span-7">
+		<div class="col-span-6 md:col-span-12 lg:col-span-6 lg:col-start-6">
 			<Body
 				markdown={blok.column2}
 				markdownRenderers={{ image: SbMarkdownImage }}
-				class="max-w-full md:max-w-170"
+				class="max-w-full gap-4 md:gap-6"
 			/>
 		</div>
 	</Grid>
